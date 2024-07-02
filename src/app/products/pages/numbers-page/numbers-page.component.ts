9d966d1ac2b431263de41929b7cdce456d7fdc11
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-01 23:24:45 -0600

    feat: decimal, percent and currency pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
-
+  public totalSells:number = 2567789.5567;
+  public percent   :number =       0.4856;
 }
