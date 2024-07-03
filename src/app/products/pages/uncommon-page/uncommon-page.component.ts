1c276ae650a49b6c49e9ce520def6f116ef18eb0
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-03 09:13:05 -0600

    feat: i18nPluralPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -14,10 +14,21 @@ export class UncommonPageComponent {
     male:'invitarlo',
     female:'invitarla'
   }
-
-  changeClient() {
+  changeClient():void {
     this.name = 'Citlalli';
     this.gender = 'female';
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Maria','Pedro','Fernando','Hernando','Eduardo','Melissa','Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+  deleteClient():void {
+    this.clients.shift();
   }
 
 }
