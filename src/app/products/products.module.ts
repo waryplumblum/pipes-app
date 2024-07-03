0479446d6712aeb9a7873f8aa92501b0ec755961
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-03 16:51:46 -0600

    feat: canFlyPipe

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,7 +7,9 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
+
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly-case.pipe';
 
 
 @NgModule({
@@ -18,6 +20,7 @@ import { ToggleCasePipe } from './pipes/toggle-case.pipe';
     OrderComponent,
     // Pipes
     ToggleCasePipe,
+    CanFlyPipe
   ],
   imports: [
     CommonModule,
